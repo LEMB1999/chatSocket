163e73debe20ec4231da7af73d92c6cb71992706
Enrique <ebelmarez1998@hotmail.com>
2021-01-14 10:56:41 -0600

    para modificar el layout y que se pueda ver los mensajes bien en la pantalla

--- app/src/main/java/teclag/c17130804/appchat/MainActivity.java
+++ app/src/main/java/teclag/c17130804/appchat/MainActivity.java
@@ -81,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
                                 txtMensaje.setGravity(Gravity.RIGHT);
                                 txtMensaje.setTextSize(20);
                                 chat.addView(txtMensaje);
+                                mensaje.setText("");
+                                mensaje.requestFocus();
                             }
                         });
 
